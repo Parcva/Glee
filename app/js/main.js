17c1ab76d3765485da24680061017a9b2387065d
Parcva <parcvania.l1@mail.ru>
2023-08-21 15:35:16 +0400

    desing

--- app/js/main.js
+++ app/js/main.js
@@ -5,6 +5,16 @@ $(function () {
     arrows: false
   });
 
-  var mixer = mixitup('.product__album');
+  var containerEl1 = document.querySelector('[data-ref="container-1"]');
+  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+
+  var config = {
+    controls: {
+      scope: 'local'
+    }
+  };
+
+  var mixer1 = mixitup(containerEl1, config);
+  var mixer2 = mixitup(containerEl2, config);
 
 });
