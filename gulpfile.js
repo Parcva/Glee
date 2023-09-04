a9676ec21863d25438982702a0886d6ded132a5b
Parcva <parcvania.l1@mail.ru>
2023-09-04 16:42:37 +0400

    add main module

--- gulpfile.js
+++ gulpfile.js
@@ -6,12 +6,23 @@ const uglify = require('gulp-uglify-es').default;
 const browserSync = require('browser-sync').create();
 const autoprefixer = require('gulp-autoprefixer');
 const clean = require('gulp-clean');
+const rename = require('gulp-rename');
 const imagemin = require('gulp-imagemin');
+const nunjucksRend = require('gulp-nunjucks-render');
+
+function nunjucks() {
+  return src('app/*.njk')
+    .pipe(nunjucksRend())
+    .pipe(dest('app'))
+    .pipe(browserSync.stream())
+}
 
 function styles() {
-  return src('app/scss/style.scss')
+  return src('app/scss/*.scss')
     .pipe(autoprefixer({ overrideBrowserslist: ['last 10 version'] }))
-    .pipe(concat('style.min.css'))
+    .pipe(rename({
+      suffix: '.min'
+    }))
     .pipe(scss({ outputStyle: 'compressed' }))
     .pipe(dest('app/css'))
     .pipe(browserSync.stream())
@@ -34,9 +45,13 @@ function scripts() {
 }
 
 function watching() {
-  watch(['app/scss/*.scss'], styles)
-  watch(['app/js/main.js'], scripts)
-  watch(['app/*.html']).on('change', browserSync.reload)
+  watch([
+    'app/scss/*.scss',
+    'app/**/*.scss'
+  ], styles);
+  watch(['app/js/main.js'], scripts);
+  watch(['app/*.njk'], nunjucks);
+  watch(['app/*.html']).on('change', browserSync.reload);
 }
 
 function images() {
@@ -82,5 +97,6 @@ exports.scripts = scripts;
 exports.watching = watching;
 exports.browsersync = browsersync;
 exports.images = images;
+exports.nunjucks = nunjucks;
 exports.build = series(cleanDist, images, building);
-exports.default = parallel(styles, scripts, browsersync, watching);
+exports.default = parallel(nunjucks, styles, scripts, browsersync, watching);
